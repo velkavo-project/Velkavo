ba0394df90e3ab2f9659688b6860d374eb0ffe28
akshatsirohi <akshatsirohi@users.noreply.github.com>
2026-04-13 06:38:00 +0530

    feat: generate unique Velkavo network IDs for mainnet/testnet/stagenet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -235,7 +235,7 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 19081;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 19082;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x41, 0x8B, 0x3C, 0x3F, 0xB8, 0x20, 0x42, 0x0A, 0x99, 0x35, 0xD9, 0x69, 0x4E, 0x9B, 0x28, 0x2C
     } }; // Bender's nightmare
   std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
   uint32_t const GENESIS_NONCE = 10000;
@@ -278,7 +278,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 29081;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 29082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0x25, 0xDE, 0x3E, 0x17, 0x6B, 0x94, 0x42, 0xF3, 0x87, 0x39, 0xC1, 0xC1, 0x1A, 0x87, 0xC3, 0x4B
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
     uint32_t const GENESIS_NONCE = 10001;
@@ -293,7 +293,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 39081;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 39082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
+        0xD7, 0xAB, 0x54, 0xD5, 0x76, 0x85, 0x46, 0x84, 0xB4, 0xEA, 0xC4, 0xDC, 0x26, 0x45, 0x6E, 0xEC
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
     uint32_t const GENESIS_NONCE = 10002;
